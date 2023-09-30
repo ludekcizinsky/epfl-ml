74184852c533a1888ff209f7b514ed36796baa1c
LudekCizinsky <ludekcizinsky11@gmail.com>
2023-09-30 21:00:00 +0200

    Finsihed lab 2

--- labs/ex02/costs.py
+++ labs/ex02/costs.py
@@ -4,19 +4,21 @@
 import numpy as np
 
 
-def compute_loss(y, tx, w):
+def compute_loss(y, tx, w, name="MSE"):
     """Calculate the loss using either MSE or MAE.
 
     Args:
-        y: shape=(N, )
-        tx: shape=(N,2)
-        w: shape=(2,). The vector of model parameters.
+        y: numpy array of shape=(N, )
+        tx: numpy array of shape=(N,2)
+        w: numpy array of shape=(2,). The vector of model parameters.
 
     Returns:
         the value of the loss (a scalar), corresponding to the input parameters w.
     """
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # TODO: compute loss by MSE
-    # ***************************************************
-    raise NotImplementedError
+    N = tx.shape[0]
+    if name == "MSE":
+        return np.sum((y - tx @ w)**2)/2*N
+    elif name == "MAE":
+        return np.sum(np.absolute((y - tx @ w)))/2*N
+    else:
+        raise NotImplementedError 
