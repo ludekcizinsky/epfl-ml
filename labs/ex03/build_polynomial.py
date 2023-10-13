59de382360e9b7e5672448fb32ed2204dc2c20d7
LudekCizinsky <ludekcizinsky11@gmail.com>
2023-10-13 11:52:46 +0200

    own solution to the ex3

---
+++ labs/ex03/build_polynomial.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""implement a polynomial basis function."""
+
+import numpy as np
+
+def build_poly(x, degree):
+    """polynomial basis functions for input data x, for j=0 up to j=degree.
+
+    Args:
+        x: numpy array of shape (N,), N is the number of samples.
+        degree: integer.
+
+    Returns:
+        poly: numpy array of shape (N,d+1)
+
+    >>> build_poly(np.array([0.0, 1.5]), 2)
+    array([[1.  , 0.  , 0.  ],
+           [1.  , 1.5 , 2.25]])
+    """
+
+    n = x.shape[0]
+    poly = np.empty((n, degree + 1))
+    for j in range(degree + 1):
+       poly[:, j] = x**j
+
+    return poly
